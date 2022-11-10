6274ebfc981f3a42bc4dddd7478741deb4a03cb4
helmer <helmer@dalt.cl>
2022-11-09 23:30:23 -0300

    se modifica asistente para que abra postman como ejemplo

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#1 import libreries
 import pyaudio
 import nltk
 nltk.download('omw-1.4')
@@ -6,101 +5,20 @@ from neuralintents import GenericAssistant
 import speech_recognition 
 import pyttsx3 as tts
 import sys
-
-#2 define assistant
+import os
+import subprocess
 
 recognizer = speech_recognition.Recognizer()
 
-""""Gets a reference to an engine instance that will use the given driver. If the requested driver is already in use by another engine instance, that engine is returned. Otherwise, a new engine is created."""
 speaker = tts.init()
-
-"""setProperty(name, value) → None
-Queues a command to set an engine property. The new property value affects all utterances queued after this command.
-
-Parameters:	
-name – Name of the property to change.
-value – Value to set.
-The following property names are valid for all drivers.
-
-rates
-Integer speech rate in words per minute.
-
-voice
-String identifier of the active voice.
-
-volume
-Floating point volume in the range of 0.0 to 1.0 inclusive."""
-
 speaker.setProperty('rate', 150)
 
-todo_list = ['ir de compras', 'ir al gimnasio']
 
-def create_note():
-    global recognizer
-    speaker.say('Que nota deseas crear?')
+def open_postman():
+    speaker.say('Abriendo Postman')
     speaker.runAndWait()
-
-    done = False 
-
-    while not done:
-        try:
-            with speech_recognition.Microphone() as mic :
-                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-                audio = recognizer.listen(mic)
-                note = recognizer.recognize_google(audio)
-                note = note.lower()
-
-                speaker.say('Escoje un nombre de archivo')
-                speaker.runAndWait()
-
-                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-                audio = recognizer.listen(mic)
-                audio = recognizer.listen(mic)
-
-                filename = recognizer.recognize_google(audio)
-                filename = note.lower()
-
-                with open(filename, 'w') as f:
-                    f.write(note)
-                    done = True
-                    speaker.say('Created')
-                    speaker.runAndWait()
-
-        except speech_recognition.UnknownValueError:
-            speaker.say('Sorry, I did not get that')
-            speaker.runAndWait()
-
-def add_to():
-    global recognizer
-    speaker.say('Quieres agregar algo a tu lista?')
-    speaker.runAndWait()
-    done = False 
-
-    while not done:
-        try:
-            with speech_recognition.Microphone() as mic:
-                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-                audio = recognizer.listen(mic)
-
-                item = recognizer.recognize_google(audio)
-                item = item.lower()
-
-                todo_list.append(item)
-                done = True
-
-                speaker.say('Agregado')   
-                speaker.runAndWait()
-
-        except speech_recognition.UnknownValueError:
-            speaker.say('Sorry, I did not get that')
-            speaker.runAndWait()
-
-def show_todos():
-    speaker.say('Esta es tu lista de tareas')
-    for item in todo_list:
-        speaker.say(item)
-    speaker.runAndWait()
-
+    user = os.getlogin()
+    subprocess.Popen('C:\\Users\\'+user+'\\AppData\\Local\\Postman\\Postman.exe')
 
 def hello():
     speaker.say('Hola, bienvenido a tu asistente personal')
@@ -113,18 +31,14 @@ def bye():
 
 
 mappings = {
-    "saludo": hello,
     "despedida": bye,
-    "crea_nota": create_note,
-    "agregar_tarea": add_to,
-    "muestra_todo": show_todos
+     "postman": open_postman
 }
 
-assistant = GenericAssistant('intents.json', intent_methods=mappings)
+assistant = GenericAssistant('intents.json', intent_methods=mappings, )
 
 assistant.train_model()
 
-#assistant.request('how are you ??')
 
 while True:
     try:
